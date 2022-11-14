7d2736397d6833fed16e714733509154c68a6396
Soumya Subhro Datta <soumya_datta@persistent.com>
2022-11-14 16:32:19 +0530

    initial status

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookFlightComponent } from './book-flight/book-flight.component';
+import { ViewDetailsComponent } from './view-details/view-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookFlightComponent,
+    ViewDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
